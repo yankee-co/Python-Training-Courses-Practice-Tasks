f5f9c8ef7ea1d0536fbb145d8cea27a894dac768
Eduard Zemlyanskyi <zimae.9525@gmail.com>
2020-11-09 23:26:20 +0200

    09-11 Homewrok

---
+++ (18) 09-11/genfunc.py
@@ -0,0 +1,6 @@
+def oddGen(start, end):
+    for i in range(start, end + 1):
+        if i % 2 == 0: yield i
+
+for i in oddGen(30, 50):
+    print(i)
---
+++ (18) 09-11/iterators.py
@@ -0,0 +1,21 @@
+class FileIterator():
+
+    def __init__(self, file):
+        try:
+            self.opened = open(f"{file}", "r")
+        except FileNotFoundError:
+            print('StopIteration occured, file not found.')
+            raise StopIteration
+        self.lines = self.opened.read().split('\n')
+        self.counter = -1
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        self.counter+=1
+        if self.counter < len(self.lines):
+            return self.lines[self.counter]
+        else:
+            self.opened.close()
+            raise StopIteration
